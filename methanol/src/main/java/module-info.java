67dd218551c985c06baf073082d0d56ce59aee1f
Moataz abdelnasser <mizo.maicon@gmail.com>
2019-12-12 05:45:31 +0200

    Add BodyDecoder.Factory providers for deflate and gzip

--- methanol/src/main/java/module-info.java
+++ methanol/src/main/java/module-info.java
@@ -1,10 +1,12 @@
-import com.github.mizosoft.methanol.BodyDecoder;
-
 module methanol {
   requires transitive java.net.http;
   requires org.checkerframework.checker.qual;
 
   exports com.github.mizosoft.methanol;
 
-  uses BodyDecoder.Factory;
+  uses com.github.mizosoft.methanol.BodyDecoder.Factory;
+
+  provides com.github.mizosoft.methanol.BodyDecoder.Factory with
+      com.github.mizosoft.methanol.internal.dec.DeflateBodyDecoderFactory,
+      com.github.mizosoft.methanol.internal.dec.GzipBodyDecoderFactory;
 }
