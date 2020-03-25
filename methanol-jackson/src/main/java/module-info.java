75534e5fbae5a4c77042611ead554a9b9e213c76
Moataz Abdelnasser <moataz.nasser20@gmail.com>
2020-03-25 02:08:36 +0200

    Add root javadoc task

--- methanol-jackson/src/main/java/module-info.java
+++ methanol-jackson/src/main/java/module-info.java
@@ -1,3 +1,32 @@
+/*
+ * Copyright (c) 2019, 2020 Moataz Abdelnasser
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * Provides {@link com.github.mizosoft.methanol.BodyAdapter.Encoder} and {@link
+ * com.github.mizosoft.methanol.BodyAdapter.Decoder} implementations for JSON using the Jackson
+ * library. Note that, for the sake of configurability, the adapters are not service-provided by
+ * default. You will need to explicitly declare service-providers that delegate to the instances
+ * created by {@link com.github.mizosoft.methanol.adapter.jackson.JacksonBodyAdapterFactory}.
+ */
 module methanol.adapter.jackson {
   requires transitive methanol;
   requires transitive com.fasterxml.jackson.databind;
--- methanol/src/main/java/module-info.java
+++ methanol/src/main/java/module-info.java
@@ -1,3 +1,33 @@
+/*
+ * Copyright (c) 2019, 2020 Moataz Abdelnasser
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/**
+ * Core Methanol module.
+ *
+ * @uses com.github.mizosoft.methanol.BodyDecoder.Factory
+ * @uses com.github.mizosoft.methanol.BodyAdapter.Encoder
+ * @uses com.github.mizosoft.methanol.BodyAdapter.Decoder
+ * @provides com.github.mizosoft.methanol.BodyDecoder.Factory For the gzip and deflate encodings.
+ */
 module methanol {
   requires transitive java.net.http;
   requires static org.checkerframework.checker.qual;
