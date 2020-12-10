52fd054d3df8d054c48cd2e7f1f5315e0f55eaa2
Moataz Abdelnasser <moataz.nasser20@gmail.com>
2020-12-10 18:04:46 +0200

    Mutated RawResponse copies

--- methanol/src/main/java/com/github/mizosoft/methanol/internal/cache/CacheResponse.java
+++ methanol/src/main/java/com/github/mizosoft/methanol/internal/cache/CacheResponse.java
@@ -2,19 +2,38 @@ package com.github.mizosoft.methanol.internal.cache;
 
 import com.github.mizosoft.methanol.internal.cache.Store.Editor;
 import com.github.mizosoft.methanol.internal.cache.Store.Viewer;
+import com.github.mizosoft.methanol.internal.extensions.ResponseBuilder;
+import com.github.mizosoft.methanol.internal.extensions.TrackedResponse;
+import java.io.Closeable;
+import java.nio.ByteBuffer;
+import java.util.List;
 import java.util.concurrent.Executor;
+import java.util.concurrent.Flow.Publisher;
+import java.util.function.Consumer;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 /** A {@code RawResponse} retrieved from cache. */
-public final class CacheResponse extends PublisherResponse {
+public final class CacheResponse extends PublisherResponse implements Closeable {
   private final Viewer viewer;
 
   public CacheResponse(CacheResponseMetadata metadata, Viewer viewer, Executor executor) {
-    super(metadata.toResponseBuilder().build(), new CacheReadingPublisher(viewer, executor));
+    this(metadata.toResponseBuilder().build(), new CacheReadingPublisher(viewer, executor), viewer);
+  }
+
+  private CacheResponse(TrackedResponse<?> response, Publisher<List<ByteBuffer>> body, Viewer viewer) {
+    super(response, body);
     this.viewer = viewer;
   }
 
-  public void dispose() {
+  @Override
+  public CacheResponse with(Consumer<ResponseBuilder<?>> mutator) {
+    var builder = ResponseBuilder.newBuilder(response);
+    mutator.accept(builder);
+    return new CacheResponse(builder.build(), publisher, viewer);
+  }
+
+  @Override
+  public void close() {
     viewer.close(); // TODO close quietly
   }
 
--- methanol/src/main/java/com/github/mizosoft/methanol/internal/cache/PublisherResponse.java
+++ methanol/src/main/java/com/github/mizosoft/methanol/internal/cache/PublisherResponse.java
@@ -1,8 +1,8 @@
 package com.github.mizosoft.methanol.internal.cache;
 
 import com.github.mizosoft.methanol.internal.extensions.ImmutableResponseInfo;
-import com.github.mizosoft.methanol.internal.extensions.TrackedResponse;
 import com.github.mizosoft.methanol.internal.extensions.ResponseBuilder;
+import com.github.mizosoft.methanol.internal.extensions.TrackedResponse;
 import java.net.http.HttpResponse.BodyHandler;
 import java.net.http.HttpResponse.BodySubscriber;
 import java.nio.ByteBuffer;
@@ -10,12 +10,13 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Flow.Publisher;
+import java.util.function.Consumer;
 
 class PublisherResponse extends RawResponse {
-  private final Publisher<List<ByteBuffer>> publisher;
+  final Publisher<List<ByteBuffer>> publisher;
 
   PublisherResponse(TrackedResponse<?> response, Publisher<List<ByteBuffer>> publisher) {
-    super(response);
+    super(dropBody(response));
     this.publisher = publisher;
   }
 
@@ -29,5 +30,18 @@ class PublisherResponse extends RawResponse {
     return subscriberFuture
         .thenComposeAsync(BodySubscriber::getBody, executor)
         .thenApply(body -> ResponseBuilder.newBuilder(response).body(body).build());
+  }
+
+  @Override
+  public RawResponse with(Consumer<ResponseBuilder<?>> mutator) {
+    var builder = ResponseBuilder.newBuilder(response);
+    mutator.accept(builder);
+    return new PublisherResponse(builder.build(), publisher);
+  }
+
+  private static TrackedResponse<?> dropBody(TrackedResponse<?> response) {
+    return response.body() != null
+        ? ResponseBuilder.newBuilder(response).dropBody().build()
+        : response;
   }
 }
