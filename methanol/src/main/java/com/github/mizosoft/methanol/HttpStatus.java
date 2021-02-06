6ea59657315742940906cd2fb1861aba845705ee
Moataz Abdelnasser <moataz.nasser20@gmail.com>
2021-02-06 17:48:42 +0200

    Utility functions for testing status codes

---
+++ methanol/src/main/java/com/github/mizosoft/methanol/HttpStatus.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2019-2021 Moataz Abdelnasser
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.mizosoft.methanol;
+
+import static com.github.mizosoft.methanol.HttpStatus.StatusKind.CLIENT_ERROR;
+import static com.github.mizosoft.methanol.HttpStatus.StatusKind.INFORMATIONAL;
+import static com.github.mizosoft.methanol.HttpStatus.StatusKind.REDIRECTION;
+import static com.github.mizosoft.methanol.HttpStatus.StatusKind.SERVER_ERROR;
+import static com.github.mizosoft.methanol.HttpStatus.StatusKind.SUCCESSFUL;
+
+import java.net.http.HttpResponse;
+
+/** Static functions for checking response status codes. */
+public class HttpStatus {
+  private HttpStatus() {}
+
+  /** Returns {@code true} if {@code statusCode} is a 1xx informational status code. */
+  public static boolean isInformational(int statusCode) {
+    return INFORMATIONAL.includes(statusCode);
+  }
+
+  /** Returns {@code true} if {@code response.statusCode()} is a 1xx informational status code. */
+  public static boolean isInformational(HttpResponse<?> response) {
+    return isInformational(response.statusCode());
+  }
+
+  /** Returns {@code true} if {@code statusCode} is a 2xx success status code. */
+  public static boolean isSuccessful(int statusCode) {
+    return SUCCESSFUL.includes(statusCode);
+  }
+
+  /** Returns {@code true} if {@code response.statusCode()} is a 2xx successful status code. */
+  public static boolean isSuccessful(HttpResponse<?> response) {
+    return isSuccessful(response.statusCode());
+  }
+
+  /** Returns {@code true} if {@code statusCode} is a 3xx redirection status code. */
+  public static boolean isRedirection(int statusCode) {
+    return REDIRECTION.includes(statusCode);
+  }
+
+  /** Returns {@code true} if {@code response.statusCode()} is a 3xx redirection status code. */
+  public static boolean isRedirection(HttpResponse<?> response) {
+    return isRedirection(response.statusCode());
+  }
+
+  /** Returns {@code true} if {@code statusCode} is a 4xx client error status code. */
+  public static boolean isClientError(int statusCode) {
+    return CLIENT_ERROR.includes(statusCode);
+  }
+
+  /** Returns {@code true} if {@code response.statusCode()} is a 4xx client error status code. */
+  public static boolean isClientError(HttpResponse<?> response) {
+    return isClientError(response.statusCode());
+  }
+
+  /** Returns {@code true} if {@code statusCode} is a 5xx server error status code. */
+  public static boolean isServerError(int statusCode) {
+    return SERVER_ERROR.includes(statusCode);
+  }
+
+  /** Returns {@code true} if {@code response.statusCode()} is a 5xx server error status code. */
+  public static boolean isServerError(HttpResponse<?> response) {
+    return isServerError(response.statusCode());
+  }
+
+  enum StatusKind {
+    INFORMATIONAL(100),
+    SUCCESSFUL(200),
+    REDIRECTION(300),
+    CLIENT_ERROR(400),
+    SERVER_ERROR(500);
+
+    private final int from;
+    private final int to;
+
+    StatusKind(int from) {
+      this(from, from + 99);
+    }
+
+    StatusKind(int from, int to) {
+      this.from = from;
+      this.to = to;
+    }
+
+    boolean includes(int statusCode) {
+      return statusCode >= from && statusCode <= to;
+    }
+  }
+}
---
+++ methanol/src/test/java/com/github/mizosoft/methanol/HttpStatusTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2019-2021 Moataz Abdelnasser
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.github.mizosoft.methanol;
+
+import static com.github.mizosoft.methanol.HttpStatus.isClientError;
+import static com.github.mizosoft.methanol.HttpStatus.isInformational;
+import static com.github.mizosoft.methanol.HttpStatus.isRedirection;
+import static com.github.mizosoft.methanol.HttpStatus.isServerError;
+import static com.github.mizosoft.methanol.HttpStatus.isSuccessful;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class HttpStatusTest {
+  @Test
+  void informational() {
+    assertTrue(isInformational(100));
+    assertTrue(isInformational(150));
+    assertTrue(isInformational(199));
+    assertFalse(isInformational(200));
+    assertFalse(isInformational(99));
+    assertFalse(isInformational(Integer.MAX_VALUE));
+    assertFalse(isInformational(Integer.MIN_VALUE));
+  }
+
+  @Test
+  void successful() {
+    assertTrue(isSuccessful(200));
+    assertTrue(isSuccessful(250));
+    assertTrue(isSuccessful(299));
+    assertFalse(isSuccessful(300));
+    assertFalse(isSuccessful(199));
+    assertFalse(isSuccessful(Integer.MAX_VALUE));
+    assertFalse(isSuccessful(Integer.MIN_VALUE));
+  }
+
+  @Test
+  void redirection() {
+    assertTrue(isRedirection(300));
+    assertTrue(isRedirection(350));
+    assertTrue(isRedirection(399));
+    assertFalse(isRedirection(400));
+    assertFalse(isRedirection(299));
+    assertFalse(isRedirection(Integer.MAX_VALUE));
+    assertFalse(isRedirection(Integer.MIN_VALUE));
+  }
+
+  @Test
+  void clientError() {
+    assertTrue(isClientError(400));
+    assertTrue(isClientError(450));
+    assertTrue(isClientError(499));
+    assertFalse(isClientError(500));
+    assertFalse(isClientError(399));
+    assertFalse(isClientError(Integer.MAX_VALUE));
+    assertFalse(isClientError(Integer.MIN_VALUE));
+  }
+
+  @Test
+  void serverError() {
+    assertTrue(isServerError(500));
+    assertTrue(isServerError(550));
+    assertTrue(isServerError(599));
+    assertFalse(isServerError(600));
+    assertFalse(isServerError(499));
+    assertFalse(isServerError(Integer.MAX_VALUE));
+    assertFalse(isServerError(Integer.MIN_VALUE));
+  }
+}
