bae88c1a4ad410daba43eb49ecb08158430eed73
mizosoft <moataz.nasser20@gmail.com>
2021-05-24 15:55:37 +0000

    Deploying to gh-pages from @ mizosoft/methanol@2afa0cd06cc7e33a2c70d53952642c2f89fa8420 🚀

---
+++ api/latest/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"methanol"},{"l":"methanol.adapter.gson"},{"l":"methanol.adapter.jackson"},{"l":"methanol.adapter.jackson.flux"},{"l":"methanol.adapter.jaxb"},{"l":"methanol.adapter.protobuf"},{"l":"methanol.brotli"},{"l":"methanol.testutils"}];updateSearchResults();
---
+++ api/latest/tag-search-index.js
@@ -0,0 +1,1 @@
+tagSearchIndex = [{"l":"Deprecated API","h":"","u":"deprecated-list.html"},{"l":"Serialized Form","h":"","u":"serialized-form.html"}];updateSearchResults();
