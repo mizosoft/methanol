d7abcbedbfd2e38493fc5e8e1bec83952e2ba2ac
Moataz Abdelnasser <moataz.nasser20@gmail.com>
2022-11-19 20:12:23 +0200

    Use correct license

--- methanol-testing/src/main/java/com/github/mizosoft/methanol/testing/TestSubscriber.java
+++ methanol-testing/src/main/java/com/github/mizosoft/methanol/testing/TestSubscriber.java
@@ -20,6 +20,45 @@
  * SOFTWARE.
  */
 
+/*
+ * This code is adapted from https://githubcom/openjdk/jdk/blob/36e5ad61e63e2f1da9cf565c607db28f23622ea9/test/jdk/java/util/concurrent/tck/SubmissionPublisherTest.java#L67.
+ * The source file contained the following licenses.
+ */
+
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/*
+ * This file is available under and governed by the GNU General Public
+ * License version 2 only, as published by the Free Software Foundation.
+ * However, the following notice accompanied the original version of this
+ * file:
+ *
+ * Written by Doug Lea and Martin Buchholz with assistance from
+ * members of JCP JSR-166 Expert Group and released to the public
+ * domain, as explained at
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+
 package com.github.mizosoft.methanol.testing;
 
 import static java.util.Objects.requireNonNull;
@@ -31,8 +70,8 @@ import java.util.concurrent.Flow.Subscription;
 import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 
 /**
- * Facilitates testing Publisher implementations and the like. Adapted from
- * https://github.com/openjdk/jdk/blob/36e5ad61e63e2f1da9cf565c607db28f23622ea9/test/jdk/java/util/concurrent/tck/SubmissionPublisherTest.java#L67
+ * A {@code Subscriber} implementation that facilitates testing {@code Publisher} implementations
+ * and the like.
  */
 public class TestSubscriber<T> implements Subscriber<T> {
   public final Deque<T> items = new ArrayDeque<>();
