5cebfd6e5ff843c9cc5a4e38c10f704d46c879bd
Moataz Abdelnasser <moataz.nasser20@gmail.com>
2022-12-17 17:00:50 +0200

    Fix skipping the removal of newborn entries' editors

--- methanol-redis/src/main/resources/scripts/append.lua
+++ methanol-redis/src/main/resources/scripts/append.lua
@@ -7,10 +7,10 @@ local editorLockTtlSeconds = tonumber(ARGV[3])
 redis.log(redis.LOG_WARNING, editorId, redis.call('get', editorLockKey))
 
 -- Update expiry if enough time has passed.
-if redis.call('ttl', wipDataKey) < 0.7 * editorLockTtlSeconds then
+if redis.call('ttl', wipDataKey) < 0.5 * editorLockTtlSeconds then
   -- Make sure we're extending the expiry of the correct editor lock.
   if redis.call('get', editorLockKey) ~= editorId then
-    redis.log(redis.LOG_WARNING, "expired editor lock ", editorId, wipDataKey)
+    redis.log(redis.LOG_WARNING, "editor lock expired ", editorId, wipDataKey)
     return -1
   end
 
@@ -18,9 +18,7 @@ if redis.call('ttl', wipDataKey) < 0.7 * editorLockTtlSeconds then
   local expireAt = now + editorLockTtlSeconds
   if redis.call('expireat', editorLockKey, expireAt) == 0
       or redis.call('expireat', wipDataKey, expireAt) == 0 then
-    redis.log(redis.LOG_WARNING, "can't set expireat ", editorId, wipDataKey)
     return -1
   end
 end
-redis.log(redis.LOG_WARNING, "appended! ", data, " on ", wipDataKey)
 return redis.call('append', wipDataKey, data)
--- methanol-redis/src/main/resources/scripts/remove.lua
+++ methanol-redis/src/main/resources/scripts/remove.lua
@@ -4,20 +4,22 @@ local staleEntryTtlSeconds = ARGV[2]
 
 local entryVersion, dataVersion = unpack(
     redis.call('hmget', entryKey, 'entryVersion', 'dataVersion'))
-if not entryVersion or targetEntryVersion ~= '-1' and targetEntryVersion ~= entryVersion then
-    return false
+if targetEntryVersion ~= '-1' and targetEntryVersion ~= entryVersion then
+  return false
 end
 
-redis.call('unlink', entryKey)
-
-local dataKey = entryKey .. ':data:' .. dataVersion
-if redis.call('expire', dataKey, staleEntryTtlSeconds) == 1 then
+local removed = redis.call('unlink', entryKey) > 0
+if removed and dataVersion then
+  local dataKey = entryKey .. ':data:' .. dataVersion
+  if redis.call('expire', dataKey, staleEntryTtlSeconds) == 1 then
     redis.call('rename', dataKey, dataKey .. ':stale')
+  end
 end
 
 -- Invalidate any ongoing edit for this entry.
 local currentEditorId = redis.call('getdel', entryKey .. ':editor')
 if currentEditorId then
-    redis.call('unlink', entryKey .. ':wip_data:' .. currentEditorId)
+  redis.call('unlink', entryKey .. ':wip_data:' .. currentEditorId)
+  removed = true
 end
-return true
+return removed
