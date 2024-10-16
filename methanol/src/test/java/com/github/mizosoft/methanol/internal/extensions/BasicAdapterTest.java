c456634a21ccf3c2d91d36337b160ba5d2b05330
mizosoft <moataz.nasser20@gmail.com>
2024-10-16 20:52:13 +0300

    Refurbish BasicAdapter

--- methanol/src/test/java/com/github/mizosoft/methanol/internal/extensions/BasicAdapterTest.java
+++ methanol/src/test/java/com/github/mizosoft/methanol/internal/extensions/BasicAdapterTest.java
@@ -30,9 +30,6 @@ import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import com.github.mizosoft.methanol.MediaType;
 import com.github.mizosoft.methanol.TypeRef;
 import com.github.mizosoft.methanol.testing.ByteBufferCollector;
-import com.github.mizosoft.methanol.testing.TestException;
-import com.github.mizosoft.methanol.testing.TestSubscriber;
-import com.github.mizosoft.methanol.testing.TestSubscriberContext;
 import com.github.mizosoft.methanol.testing.TestSubscriberExtension;
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
@@ -43,21 +40,20 @@ import java.nio.ByteBuffer;
 import java.nio.charset.MalformedInputException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Flow.Publisher;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledForJreRange;
-import org.junit.jupiter.api.condition.JRE;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.io.TempDir;
 
 @ExtendWith(TestSubscriberExtension.class)
 class BasicAdapterTest {
   @Test
-  void encoding(@TempDir Path tempDir) throws IOException {
+  void encodeString() {
     var encoder = BasicAdapter.encoder();
     verifyThat(encoder).converting("Pikachu").succeedsWith("Pikachu");
     verifyThat(encoder).converting("é€Pikachu€é").succeedsWith("é€Pikachu€é");
@@ -69,22 +65,74 @@ class BasicAdapterTest {
         .converting("é€Pikachu€é")
         .withMediaType(MediaType.parse("text/plain; charset=ascii"))
         .succeedsWith("??Pikachu??");
+  }
+
+  @Test
+  void encodeByteBuffer() {
+    var encoder = BasicAdapter.encoder();
     verifyThat(encoder)
         .converting(new byte[] {1, 2, 3})
-        .succeedsWith(ByteBuffer.wrap(new byte[] {1, 2, 3}));
+        .asBodyPublisher()
+        .repeatedly(verifier -> verifier.succeedsWith(ByteBuffer.wrap(new byte[] {1, 2, 3})));
+  }
+
+  @Test
+  void encodeInputStream() {
+    var encoder = BasicAdapter.encoder();
     verifyThat(encoder)
         .converting(new ByteArrayInputStream("Pikachu".getBytes(UTF_8)))
         .succeedsWith("Pikachu");
-    verifyThat(encoder)
-        .<Supplier<InputStream>>converting(
-            () -> new ByteArrayInputStream("Pikachu".getBytes(UTF_8)))
-        .succeedsWith("Pikachu");
-    verifyThat(encoder)
-        .converting(List.of(new byte[] {1}, new byte[] {1, 2}, new byte[] {1, 2, 3}))
-        .succeedsWith(ByteBuffer.wrap(new byte[] {1, 1, 2, 1, 2, 3}));
+  }
 
+  @Test
+  void encodeInputStreamSupplier() {
+    var encoder = BasicAdapter.encoder();
+    verifyThat(encoder)
+        .converting(
+            () -> new ByteArrayInputStream("Pikachu".getBytes(UTF_8)),
+            new TypeRef<Supplier<InputStream>>() {})
+        .asBodyPublisher()
+        .repeatedly(verifier -> verifier.succeedsWith("Pikachu"));
+  }
+
+  @Test
+  void encodeByteArrayIterable() {
+    var encoder = BasicAdapter.encoder();
+    verifyThat(encoder)
+        .converting(
+            List.of(new byte[] {1}, new byte[] {1, 2}, new byte[] {1, 2, 3}),
+            new TypeRef<Iterable<byte[]>>() {})
+        .asBodyPublisher()
+        .repeatedly(
+            verifier -> verifier.succeedsWith(ByteBuffer.wrap(new byte[] {1, 1, 2, 1, 2, 3})));
+
+    // Pass a generic subtype.
+    verifyThat(encoder)
+        .converting(
+            List.of(new byte[] {1}, new byte[] {1, 2}, new byte[] {1, 2, 3}), new TypeRef<>() {})
+        .asBodyPublisher()
+        .repeatedly(
+            verifier -> verifier.succeedsWith(ByteBuffer.wrap(new byte[] {1, 1, 2, 1, 2, 3})));
+
+    // Pass a raw subtype.
+    class ByteArrayList extends ArrayList<byte[]> {
+      ByteArrayList(List<byte[]> list) {
+        super(list);
+      }
+    }
+    verifyThat(encoder)
+        .converting(
+            new ByteArrayList(List.of(new byte[] {1}, new byte[] {1, 2}, new byte[] {1, 2, 3})))
+        .asBodyPublisher()
+        .repeatedly(
+            verifier -> verifier.succeedsWith(ByteBuffer.wrap(new byte[] {1, 1, 2, 1, 2, 3})));
+  }
+
+  @Test
+  void encodeFile(@TempDir Path tempDir) throws IOException {
     var file = Files.createTempFile(tempDir, BasicAdapterTest.class.getName(), "");
     Files.writeString(file, "Pikachu");
+    var encoder = BasicAdapter.encoder();
     verifyThat(encoder).converting(file).succeedsWith("Pikachu");
   }
 
@@ -92,54 +140,48 @@ class BasicAdapterTest {
   void unsupportedEncoding() {
     verifyThat(BasicAdapter.encoder()).converting(1).isNotSupported();
 
-    // For non-scalars the UnsupportedOperation is thrown during transmission.
+    verifyThat(BasicAdapter.encoder()).<Supplier<Integer>>converting(() -> 1).isNotSupported();
     verifyThat(BasicAdapter.encoder())
-        .<Supplier<Integer>>converting(() -> 1)
-        .failsWith(UnsupportedOperationException.class);
-  }
+        .converting(() -> 1, new TypeRef<Supplier<Integer>>() {})
+        .isNotSupported();
 
-  @Test
-  @EnabledForJreRange(
-      min = JRE.JAVA_18) // Earlier JDK versions weren't catching exceptions from Iterator::next.
-  void unsupportedEncoding_byteArrayIterable() {
+    verifyThat(BasicAdapter.encoder()).converting(List.of(1, 2, 3)).isNotSupported();
     verifyThat(BasicAdapter.encoder())
-        .converting(List.of(1, 2, 3))
-        .failsWith(UnsupportedOperationException.class);
-  }
+        .converting(List.of(1, 2, 3), new TypeRef<Iterable<Integer>>() {})
+        .isNotSupported();
 
-  @Test
-  void failingInputStreamSupplier() {
+    class IntegerList extends ArrayList<Integer> {
+      IntegerList(List<Integer> list) {
+        super(list);
+      }
+    }
     verifyThat(BasicAdapter.encoder())
-        .<Supplier<InputStream>>converting(
-            () -> {
-              throw new TestException();
-            })
-        .failsWith(TestException.class);
+        .converting(new IntegerList(List.of(1, 2, 3)))
+        .isNotSupported();
+    verifyThat(BasicAdapter.encoder())
+        .converting(new IntegerList(List.of(1, 2, 3)))
+        .isNotSupported();
   }
 
   @Test
-  void failingSubscriberForFailingInputStreamSupplier(TestSubscriber<ByteBuffer> subscriber) {
-    Supplier<InputStream> supplier =
-        () -> {
-          throw new TestException();
-        };
-    var publisher = BasicAdapter.encoder().toBody(supplier, MediaType.ANY);
-    subscriber.throwOnSubscribe(true);
-    publisher.subscribe(subscriber);
-    assertThat(subscriber.awaitError())
-        .isInstanceOf(TestException.class)
-        .hasSuppressedException(new TestException());
-  }
-
-  @Test
-  void decoding(TestSubscriberContext subscriberContext) {
+  void decodeString() {
     var decoder = BasicAdapter.decoder();
     verifyThat(decoder).converting(String.class).withBody("Pikachu").succeedsWith("Pikachu");
+  }
+
+  @Test
+  void decodeInputStream() {
+    var decoder = BasicAdapter.decoder();
     verifyThat(decoder)
         .converting(InputStream.class)
         .withBody("Pikachu")
         .completedBody()
         .satisfies(in -> assertThat(new String(in.readAllBytes(), UTF_8)).isEqualTo("Pikachu"));
+  }
+
+  @Test
+  void decodeReader() {
+    var decoder = BasicAdapter.decoder();
     verifyThat(decoder)
         .converting(Reader.class)
         .withBody("Pikachu")
@@ -168,20 +210,40 @@ class BasicAdapterTest {
             reader ->
                 assertThatExceptionOfType(MalformedInputException.class)
                     .isThrownBy(() -> new BufferedReader(reader).readLine()));
+  }
+
+  @Test
+  void decodeByteArray() {
+    var decoder = BasicAdapter.decoder();
     verifyThat(decoder)
         .converting(byte[].class)
         .withBody(ByteBuffer.wrap(new byte[] {1, 2, 3}))
         .succeedsWith(new byte[] {1, 2, 3});
+  }
+
+  @Test
+  void decodeByteBuffer() {
+    var decoder = BasicAdapter.decoder();
     verifyThat(decoder)
         .converting(ByteBuffer.class)
         .withBody(ByteBuffer.wrap(new byte[] {1, 2, 3}))
         .succeedsWith(ByteBuffer.wrap(new byte[] {1, 2, 3}));
+  }
+
+  @Test
+  void decodeLines() {
+    var decoder = BasicAdapter.decoder();
     verifyThat(decoder)
         .converting(new TypeRef<Stream<String>>() {})
         .withBody("A\nB\nC\r\nD")
         .completedBody()
         .asInstanceOf(InstanceOfAssertFactories.STREAM)
         .containsExactly("A", "B", "C", "D");
+  }
+
+  @Test
+  void decodePublisher() {
+    var decoder = BasicAdapter.decoder();
     verifyThat(decoder)
         .converting(new TypeRef<Publisher<List<ByteBuffer>>>() {})
         .withBody("Pikachu")
@@ -190,6 +252,11 @@ class BasicAdapterTest {
             publisher ->
                 assertThat(UTF_8.decode(ByteBufferCollector.collectMulti(publisher)).toString())
                     .isEqualTo("Pikachu"));
+  }
+
+  @Test
+  void decodeVoid() {
+    var decoder = BasicAdapter.decoder();
     verifyThat(decoder).converting(Void.class).withBody("Pikachu").succeedsWith(null);
   }
 
