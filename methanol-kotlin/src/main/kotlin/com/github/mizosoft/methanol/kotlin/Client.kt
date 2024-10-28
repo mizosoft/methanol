d4264e44a1d153731dac097d77e033ec5a8971ff
mizosoft <moataz.nasser20@gmail.com>
2024-10-28 22:58:47 +0300

    Add license

--- methanol-kotlin/src/main/kotlin/com/github/mizosoft/methanol/kotlin/Client.kt
+++ methanol-kotlin/src/main/kotlin/com/github/mizosoft/methanol/kotlin/Client.kt
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2024 Moataz Abdelnasser
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.github.mizosoft.methanol.kotlin
 
 import com.github.mizosoft.methanol.AdapterCodec
@@ -219,36 +241,6 @@ internal fun BaseRequestSpec.attachCoroutineScope(coroutineContext: CoroutineCon
   }
 }
 
-/** Fetches the response to sending the given request, mapping the response body into [T]. */
-suspend inline fun <reified T> Client.fetch(request: Request): Response<T> {
-  var finalRequest = request
-  if (interceptors().isNotEmpty() || backendInterceptors().isNotEmpty()) {
-    val localCoroutineContext = coroutineContext
-    finalRequest = Request(request) {
-      attachCoroutineScope(localCoroutineContext)
-    }
-  }
-  return send(finalRequest, TypeRef<T>())
-}
-
-/**
- * Fetches the response sending a request to the given URI, mapping the response body with the given
- * [BodyHandler]. The request has a GET method by default. A spec block can be optionally
- * passed to customize the request.
- */
-suspend fun <T> Client.fetch(
-  uri: String,
-  bodyHandler: BodyHandler<T>,
-  block: RequestSpec.() -> Unit = {}
-): Response<T> = sendAsync(
-  createRequestWithCoroutineScope(
-    uri,
-    coroutineContext,
-    RequestFactorySpec(),
-    block
-  ), bodyHandler
-).await()
-
 @PublishedApi
 internal suspend fun <T> Client.fetch(
   uri: String,
@@ -277,6 +269,36 @@ internal suspend fun <T> Client.fetch(
     block
   ),
   typeRef
+).await()
+
+/** Fetches the response to sending the given request, mapping the response body into [T]. */
+suspend inline fun <reified T> Client.fetch(request: Request): Response<T> {
+  var finalRequest = request
+  if (interceptors().isNotEmpty() || backendInterceptors().isNotEmpty()) {
+    val localCoroutineContext = coroutineContext
+    finalRequest = Request(request) {
+      attachCoroutineScope(localCoroutineContext)
+    }
+  }
+  return send(finalRequest, TypeRef<T>())
+}
+
+/**
+ * Fetches the response sending a request to the given URI, mapping the response body with the given
+ * [BodyHandler]. The request has a GET method by default. A spec block can be optionally
+ * passed to customize the request.
+ */
+suspend fun <T> Client.fetch(
+  uri: String,
+  bodyHandler: BodyHandler<T>,
+  block: RequestSpec.() -> Unit = {}
+): Response<T> = sendAsync(
+  createRequestWithCoroutineScope(
+    uri,
+    coroutineContext,
+    RequestFactorySpec(),
+    block
+  ), bodyHandler
 ).await()
 
 /**
