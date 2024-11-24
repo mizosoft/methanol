6ec6b5b7f5153b0804132dc51f5046ab677c8bfd
mizosoft <moataz.nasser20@gmail.com>
2024-11-24 19:27:21 +0200

    Use correct package for moshi adapters

--- methanol-moshi/src/test/kotlin/com/github/mizosoft/methanol/adapter/moshi/MoshiAdapterTest.kt
+++ methanol-moshi/src/test/kotlin/com/github/mizosoft/methanol/adapter/moshi/MoshiAdapterTest.kt
@@ -1,10 +1,11 @@
 package com.github.mizosoft.methanol.adapter.moshi
 
 import com.github.mizosoft.methanol.MediaType
-import com.github.mizosoft.methanol.adapters.moshi.MoshiAdapter.Companion.Decoder
-import com.github.mizosoft.methanol.adapters.moshi.MoshiAdapter.Companion.Encoder
+import com.github.mizosoft.methanol.adapter.moshi.MoshiAdapter.Companion.Decoder
+import com.github.mizosoft.methanol.adapter.moshi.MoshiAdapter.Companion.Encoder
 import com.github.mizosoft.methanol.kotlin.TypeRef
 import com.github.mizosoft.methanol.testing.verifiers.DecoderVerifier
+import com.github.mizosoft.methanol.testing.verifiers.DecoderVerifier.BodyConversionStep
 import com.github.mizosoft.methanol.testing.verifiers.Verifiers.verifyThat
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.JsonReader
@@ -34,7 +35,8 @@ object CompactPointAdapter {
   }
 }
 
-inline fun <reified T> DecoderVerifier.converting() = converting(TypeRef<T>())
+inline fun <reified T> DecoderVerifier.converting(): BodyConversionStep<T> =
+  converting(TypeRef<T>())
 
 data class Point(val x: Int, val y: Int)
 
